09bf65106892c4b948e9d17c7a87058f401aae11
Sophia Castillo <Sphcastillo@gmail.com>
2022-09-21 23:31:54 +0200

    installing Firebase

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -22,8 +22,9 @@ function Suggestions() {
                         />
                         <div className="flex-1 ml-4">
                             <h2 className="font-semibold text-sm">{profile.username}</h2>
-                            <h3>{profile.name}</h3>
+                            <h3 className="text-xs text-gray-400">{profile.name}</h3>
                         </div>
+                        <button className="text-blue-400 text-xs font-bold">Follow</button>
                     </div>
                 )
             })}
