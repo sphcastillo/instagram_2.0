040574c3fafec96e488ef587485f970bddbf3323
Sophia Castillo <Sphcastillo@gmail.com>
2022-09-22 00:09:33 +0200

    have successfully logged in after going into Google cloud

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
 
-function signIn() {
+// this is running on the browser - the client
+function signIn({ providers }) {
     return (
-        <div>
-            I am the signin page
-        </div>
+        <>
+        {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+                <button onClick={() => SignIntoProvider(provider.id)}>
+                    Sign in with {provider.name}
+                </button>
+            </div>
+        ))}            
+        </>
     )
 }
 
+// SERVER SIDE RENDERING
+// the server
+
+// adding special function 
+// used to help replace the signin page
+export async function getServerSideProps(){
+    // get our providers from [...nextauth].js --> thru getProviders()
+    // gets us back the providers we initailized everything for
+    const providers = await getProviders();
+    // back it back to the client side
+    return {
+        props: {
+            providers
+        }
+    }
+};
+
 export default signIn;
+
