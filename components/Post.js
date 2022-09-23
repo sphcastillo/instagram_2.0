df6d711a283b400262c0b62ad40f9c66c38e3697
Sophia Castillo <Sphcastillo@gmail.com>
2022-09-24 01:32:30 +0200

    Post: uploaded COMMENTS aren't showing

--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     BookmarkIcon,
     ChatIcon,
@@ -8,8 +8,53 @@ import {
     PaperAirplaneIcon,
 } from '@heroicons/react/outline';
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { useSession } from 'next-auth/react';
+import { addDoc, collection, query, onSnapshot, orderBy, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function Post({ id, username, userImg, img, caption }) {
+    const { data: session } = useSession();
+    const [comment, setComment] = useState("");
+    const [comments, setComments] = useState([]);
+    const [likes, setLikes] = useState([]);
+    const [hasLiked, setHasLiked] = useState(false);
+
+    useEffect(
+        () =>
+            onSnapshot(
+                query(
+                    collection(db, 'posts', id, 'comments'),
+                    orderBy('timestamp', 'desc')
+                ),
+                (snapshot) => setComments(snapshot.docs)
+            ),
+        [db]
+    );
+
+    useEffect(
+        () => 
+            onSnapshot(collection(db, "post", id, "likes"), 
+                (snapshot) => setLikes(snapshot.docs)
+                ),
+        []
+    );
+
+    const sendComment = async (e) => {
+        e.preventDefault();
+
+        const commentToSend = comment;
+        setComment(""); // to empty the input field
+
+        // add the comment to the database
+        await addDoc(collection(db, 'posts', id, 'comments'), {
+            comment: commentToSend,
+            username: session.user.username,
+            userImage: session.user.image,
+            timestamp: serverTimestamp()
+        })
+    }
+
+    console.log("comments:", comments);
     return (
         <div className="bg-white my-7 border rounded-sm">
 
@@ -32,14 +77,16 @@ function Post({ id, username, userImg, img, caption }) {
             />
 
             {/* Buttons */}
-            <div className="flex justify-between px-4 pt-4">
-                <div className="flex space-x-4">
-                    <HeartIcon className="btn" />
-                    <ChatIcon className="btn" />
-                    <PaperAirplaneIcon className="btn" />
+            {session && (
+                <div className="flex justify-between px-4 pt-4">
+                    <div className="flex space-x-4">
+                        <HeartIcon className="btn" />
+                        <ChatIcon className="btn" />
+                        <PaperAirplaneIcon className="btn" />
+                    </div>
+                    <BookmarkIcon className="btn" />
                 </div>
-                <BookmarkIcon className="btn" />
-            </div>
+            )}
 
             {/* caption */}
             <p className="p-5 truncate">
@@ -49,16 +96,66 @@ function Post({ id, username, userImg, img, caption }) {
 
             {/* comments */}
 
+            {comments.length > 0 && (
+                <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+                    {comments.map((comment) => {
+                        <div className="flex items-center space-x-2 mb-3" key={comment.id}>
+                            <img
+                                src={comment.data().image} 
+                                className="h-7 rounded-full"
+                                alt=""
+                            />
+                            <p>HELLO!</p>
+                        </div>
+                    })}
+                </div>
+            )}
+
+            {/* {session && 
+                comments.length > 0 && (
+                <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+                    {comments.map((comment) => {
+                        <div 
+                            key={comment.id}
+                            className="flex items-center space-x-2 mb-3"
+                        >
+                            <img 
+                                src={comment.data().userImage} 
+                                alt="" 
+                                className="h-7 rounded-full"
+                            />
+                            <p className="text-sm flex-1">
+                                <span className="font-bold">{comment.data().username}
+                                </span>{" "}
+                                {comment.data().comment}
+                            </p>
+                        </div>
+                    })}
+                </div>
+            )} */}
+
             {/* input box */}
-            <form className='flex items-center p-4'>
-                <EmojiHappyIcon className="h-7" />
-                <input 
-                    type="text" 
-                    placeholder="Add a comment..."
-                    className="border-none flex-1 focus:ring-0 outline-none" 
-                />
-                <button className="font-semibold text-blue-400">Post</button>
-            </form>
+            
+            {session && (
+                <form className='flex items-center p-4'>
+                    <EmojiHappyIcon className="h-7" />
+                    <input 
+                        type="text" 
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        placeholder="Add a comment..."
+                        className="border-none flex-1 focus:ring-0 outline-none" 
+                    />
+                    <button 
+                        className="font-semibold text-blue-400 cursor-pointer"
+                        type="submit"
+                        disabled={!comment.trim()}
+                        onClick={sendComment}
+                    >
+                    Post
+                    </button>
+                </form>
+            )}
         </div>
     )
 }
